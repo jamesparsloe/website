1710bec9918905a4673ff8515bd597418cd15bec
James Parsloe <jamesparsloe@gmail.com>
2025-05-27 12:23:25 +0100

    Metadata

--- app/writing/[slug]/page.tsx
+++ app/writing/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import { notFound } from "next/navigation";
 import { getPosts, getPost } from "@/app/utils"
 import Link from "next/link";
+import { Metadata } from "next";
 
 export async function generateStaticParams() {
     const posts = await getPosts()
@@ -15,6 +16,28 @@ type Props = {
         slug: string;
     }>
 };
+
+export async function generateMetadata(props: Props): Promise<Metadata> {
+    const { slug } = await props.params;
+    let post = await getPost(slug);
+
+    if (!post) {
+        return notFound();
+    }
+
+    return {
+        metadataBase: new URL("https://jamesparsloe.com"),
+        title: post.metadata.title,
+        description: post.metadata.description,
+        openGraph: {
+            title: post.metadata.title,
+            description: post.metadata.description,
+            type: "article",
+            url: `/writing/${slug}`,
+        },
+    }
+}
+
 
 export default async function Page(props: Props) {
     const { slug } = await props.params
